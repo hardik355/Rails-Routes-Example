9a4a7109bb5b17a760daa31fcfb8bc198343274a
Your Name <you@example.com>
2024-11-11 17:28:24 +0530

    routing added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# User.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }, { name: "Same" }])
+
+
+# User Created
+10.times do
+  User.create(
+    name: Faker::Name.name
+  )
+end
+
+
+# Blog Created
+20.times do
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 2),
+    user_id: User.pluck(:id).sample
+  )
+end
